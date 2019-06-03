b0a6903f05ffe3d79b994020a35ce12deb69837c
Cheolwan-Park <cheolwan.park552@gmail.com>
2019-06-03 13:59:16 +0900

    print time

--- interpreter.py
+++ interpreter.py
@@ -1,15 +1,25 @@
 import parse
 import evaluation
+import sys
+import time
 
 filename = "input.txt"
 file = open(filename)
 code = file.read()
 file.close()
+start_t = time.time()
 code = parse.preprocess(code)
 code = parse.parse(code)
+print("parsing time : %s seconds" % (time.time() - start_t))
+sys.setrecursionlimit(10000)
+print("recursion limit %d" % sys.getrecursionlimit())
+print("============================= results =============================")
+start_t = time.time()
 for c in code:
     val = evaluation.evaluation(c)
     if None is not val:
         if isinstance(val, float) and val.is_integer():
             val = int(val)
         print(val)
+print("============================= results =============================")
+print("evaluating time : %s seconds" % (time.time() - start_t))
